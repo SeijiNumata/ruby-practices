4e8a6271e5cfddd032444c2e539421d2d64c1756
SeijiNumata <seizi.numata@gmail.com>
2021-02-14 22:21:27 +0900

    レビュー反映　requireの先頭の./が不要だったため削除

--- 09.wc_object/main.rb
+++ 09.wc_object/main.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require_relative './lib/calc'
-require_relative './lib/input_calculator'
-require_relative './lib/wc_command'
-require_relative './lib/files_calculator'
+require_relative 'lib/calc'
+require_relative 'lib/input_calculator'
+require_relative 'lib/wc_command'
+require_relative 'lib/files_calculator'
 
 options = ARGV.getopts('l')
 wc_command = WcCommand.new
